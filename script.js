d4300c0ebbe5af88e029278fa41f1bf94b42a19c
angelia collins <angiecollins09@gmail.com>
2020-10-14 11:25:59 -0600

    added prompts

--- script.js
+++ script.js
@@ -17,10 +17,32 @@ generateBtn.addEventListener("click", writePassword);
 //When user clicks make a prompt
 generateBtn.onclick = function(){
 
-    if (confirm("Press a button!")) {
-      txt = "You pressed OK!";
+    if (prompt("How many characters do you need? Pick between 8 and 128 characters.")) {
+      
     } else {
-      txt = "You pressed Cancel!";
+      return Math.floor(Math.random() * Math.floor(max));
     }
+
+    if (confirm("Do you want lowercase letters?")) {
+
+    } 
+
+    if (confirm("Do you want uppercase letters?")) {
+
+    } 
+
+    if (confirm("Do you want numbers?")) {
+
+    } 
+
+    if (confirm("Do you want special characters?")) {
+
+    }
+
+    // if (confirm("How many characters do you need?")) {
+    //   txt = "You pressed OK!";
+    // } else {
+    //   txt = "You pressed Cancel!";
+    // }
 
 };
