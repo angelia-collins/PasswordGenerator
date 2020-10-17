00638981e5146d363aa4e950c12a61db37ab914c
angelia collins <angiecollins09@gmail.com>
2020-10-16 20:11:43 -0600

    added new character arrays

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var Arr_lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var Arr_capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var Arr_specChar = [" ", "!", "#", "$", "%", "&", "\"", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+var Arr_numOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
